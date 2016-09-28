fb465dec3f0c9e416635e2c6be4abb997f18a215
Francisco Flores <franciscocapipo@gmail.com>
2016-09-28 02:45:58 -0700

    I have fixed some bugs

--- client-module.js
+++ client-module.js
@@ -1,51 +1,48 @@
-Modules.modules = {};
-Modules.require = (...names) => {
-  if (names.length == 0) {
-    return new Promise((resolve, reject) => {resolve()});
-  }
-  function getOne(name) {
+Modules = {modules: {}};
+Modules.resolveDirname = filename => filename.split('?')[0].split('/').slice(0,-1).join('/') + '/';
+Modules.dirname = Modules.resolveDirname(window.location.href)
+Modules.require = function(...names) {
+    if (names.length == 0) {
+      return new Promise((resolve, reject) => {resolve()});
+    }
     var parent = this;
-    var url = `/${name}.js`;
-    return new Promise(function(resolve, reject){
-      var module = Modules.modules[url];
-      if (module) {
-        resolve(module.exports);
-      } else {
-        module = Modules.modules[url] = {
-          loaded: false,
-          filename: url,
-          dirname: url.split('/').slice(0,-1).join('/') + '/',
-          require: [],
-          prepare: function () {},
-        };
-        $.ajax(url, { data: {etag: etag}, dataType: 'text'})
-        .done(function (response) {
+    var getOne = (name) => {
+      var url = `${parent.dirname}${name}.js`;
+      return new Promise(function(resolve, reject){
+        var module = Modules.modules[url];
+        if (module) {
+          resolve(module.exports);
+        } else {
+          module = Modules.modules[url] = {
+            loaded: false,
+            filename: url,
+            dirname: Modules.resolveDirname(url),
+            require: [],
+            prepare: imports => null,
+            exports: {},
+          };
+
+          $.ajax(url, {
+            cache: false,
+            dataType: 'text',
+          }).done(response => {
             new Function('module', response)(module);
-            if (module.require.length) {
-              require.apply(module, module.require)
-                .then(function(imports) {
-                  module.imports = imports;
-                  module.prepare(imports);
-                  module.loaded = true;
-                  resolve(module.exports);
-                });
-            } else {
-              module.prepare();
-              module.loaded = true;
-              resolve(module.exports);
-            }
-        })
-        .fail(function (jqXHR) {
-            handleErrorResponse(jqXHR);
-            reject(jqXHR);
-        });
-      }
+            Modules.require.apply(module, module.require)
+            .then(function(imports) {
+                module.imports = imports;
+                module.prepare(imports);
+                module.loaded = true;
+                resolve(module.exports);
+            });
+          }).fail(reject);
+
+        }
+      });
+    };
+    return new Promise(function(resolve, reject) {
+      Promise.all(names.map(getOne))
+      .then(function(imports) {
+        resolve(imports);
+      });
     });
-  };
-  return new Promise(function(resolve, reject) {
-    Promise.all(_.map(names, getOne))
-    .then(function(imports) {
-      resolve(imports);
-    });
-  });
-}
+  }
