da99b27625f16c82ff31e7c003472c3e676d5235
Francisco Flores <franciscocapipo@gmail.com>
2016-09-29 00:31:52 -0700

    wrapped all in require function

--- client-module.js
+++ client-module.js
@@ -1,33 +1,34 @@
-Modules = {modules: {}};
-Modules.resolveDirname = filename => filename.split('?')[0].split('/').slice(0,-1).join('/') + '/';
-Modules.dirname = Modules.resolveDirname(window.location.href)
-Modules.require = function(...names) {
+var require = (function() {
+  var modules = {};
+  var resolveDirname = filename => filename.split('?')[0].split('/').slice(0,-1).join('/') + '/';
+  var dirname = resolveDirname(window.location.href)
+
+  var fn = function(...names) {
     if (names.length == 0) {
       return new Promise((resolve, reject) => {resolve()});
     }
     var parent = this;
     var getOne = (name) => {
-      var url = `${parent.dirname}${name}.js`;
+      var url = `${parent.dirname || dirname}${name}.js`;
       return new Promise(function(resolve, reject){
-        var module = Modules.modules[url];
+        var module = modules[url];
         if (module) {
           resolve(module.exports);
         } else {
-          module = Modules.modules[url] = {
+          module = modules[url] = {
             loaded: false,
             filename: url,
-            dirname: Modules.resolveDirname(url),
+            dirname: resolveDirname(url),
             require: [],
             prepare: imports => null,
             exports: {},
           };
-
           $.ajax(url, {
             cache: false,
             dataType: 'text',
           }).done(response => {
             new Function('module', response)(module);
-            Modules.require.apply(module, module.require)
+            fn.apply(module, module.require)
             .then(function(imports) {
                 module.imports = imports;
                 module.prepare(imports);
@@ -46,3 +47,7 @@ Modules.require = function(...names) {
       });
     });
   }
+  return fn;
+}({
+
+}));
