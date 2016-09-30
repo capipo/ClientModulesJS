2a54a055beed726ee770e926dc6ac22c449e3c82
Francisco Flores <franciscocapipo@gmail.com>
2016-09-30 00:19:09 -0700

    Now require function only accepts a single argument. Accepts string or array of strings. If it receive a string, a single exports object will be sent to callback method If it receive an array of strings, an array of exports objects will be sent to callback method

--- client-module.js
+++ client-module.js
@@ -16,6 +16,8 @@ var require = (function() {
     return newParts.join("/") || (newParts.length ? "/" : ".");
   }
 
+  var modules = {};
+
   class Module {
     constructor(url) {
       this.loaded = false;
@@ -28,19 +30,17 @@ var require = (function() {
       this.prepare = imports => null,
       this.exports = {};
     }
+
     addChildren(module) {
       this.children.push(module);
       module.parent = this;
     }
   }
 
-  var modules = {};
   modules.main = new Module(window.location.href.split('?')[0])
+  modules.main.filename = null;
 
-  var fn = function(...names) {
-    if (names.length == 0) {
-      return new Promise((resolve, reject) => {resolve()});
-    }
+  var fn = function(names) {
     var getOne = (name) => {
       var parent = this instanceof Module ? this : modules.main ;
       var url = join(parent.dirname, name + '.js');
@@ -51,15 +51,13 @@ var require = (function() {
         } else {
           module = modules[url] = new Module(url);
           parent.addChildren(module);
-          $.ajax(url, {
-            cache: false,
-            dataType: 'text',
+          $.ajax(url, { cache: false, dataType: 'text'
           }).done(response => {
             new Function('module', response)(module);
-            fn.apply(module, module.require)
+            fn.bind(module)(module.require)
             .then(function(imports) {
-                module.imports = imports;
                 module.prepare(imports);
+                delete module.prepare;
                 module.loaded = true;
                 resolve(module.exports);
             });
@@ -67,13 +65,16 @@ var require = (function() {
         }
       });
     };
-    return new Promise(function(resolve, reject) {
-      Promise.all(names.map(getOne))
-      .then(function(imports) {
-        resolve(imports);
-      });
-    });
+
+    if (Array.isArray(names)) {
+      return Promise.all(names.map(getOne));
+    } else if (typeof names === 'string') {
+      return getOne(names);
+    } else {
+      return new Promise((resolve, reject) => {resolve(names)});
+    }
   }
+  fn.modules = modules;
   return fn;
 }({
 
