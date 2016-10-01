006af5ba5525cef966a68fc20f7afc94f39f4484
Francisco Flores <franciscocapipo@gmail.com>
2016-10-01 00:12:59 -0700

    Refactoring esthetics and comments

--- client-module.js
+++ client-module.js
@@ -1,5 +1,5 @@
 var require = (function() {
-  // function copied from creationix/path.js
+  // function join copied from creationix/path.js
   function join(/* path segments */) {
     var parts = [];
     for (var i = 0, l = arguments.length; i < l; i++) {
@@ -38,16 +38,21 @@ var require = (function() {
   }
   modules.main = new Module(window.location.pathname);
 
+  // function to append .js to name  if necessary
+  var with_js = name => name + (name.substr(-3) == '.js' ? '' : '.js');
+
+  // require function
   var fn = function(names) {
     if (Array.isArray(names)) {
       return Promise.all(names.map(fn));
     } else if (typeof names === 'string') {
       var name = names;
       var parent = this instanceof Module ? this : modules.main ;
-      var with_js = name => name + (name.substr(-3) == '.js' ? '' : '.js');
       var url = join(parent.dirname, with_js(name));
+      var module = modules[url];
+      var ajaxOpts = { cache: false, dataType: 'text' };
+
       return new Promise(function(resolve, reject){
-        var module = modules[url];
         if (module) {
           if(module.loaded) {
             resolve(module.exports);
@@ -57,20 +62,23 @@ var require = (function() {
         } else {
           module = modules[url] = new Module(url);
           parent.addChildren(module);
-          $.ajax(url, { cache: false, dataType: 'text'})
-            .done(response => {
-              new Function('module', response)(module);
-              fn.bind(module)(module.require)
-                .then(function(imports) {
-                  module.prepare(imports);
-                  delete module.prepare;
-                  module.loaded = true;
-                  $(module).trigger('loaded');
-                  resolve(module.exports);
-                })
-                .catch(reject);
+          // fetching module definition
+          $.ajax(url, ajaxOpts).fail(reject)
+          .done(response => {
+            // execute module definition
+            new Function('module', response)(module);
+            // getting module require
+            fn.bind(module)(module.require).catch(reject)
+            .then(imports => {
+              // prepare after require resolved
+              module.prepare(imports);
+              delete module.prepare;
+              // finalize and resolve
+              module.loaded = true;
+              $(module).trigger('loaded');
+              resolve(module.exports);
             })
-            .fail(reject);
+          });
         }
       });
     } else {
