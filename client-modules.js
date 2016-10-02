23e074da824352d5e0517ede8bb4272cd9b8b7c4
Francisco Flores <franciscocapipo@gmail.com>
2016-10-01 19:57:31 -0700

    Se cambio el criterio para cachear los modulos, tambien se agrego la función require.basedir para especificar el directorio donde se buscarán los modulos

--- client-modules.js
+++ client-modules.js
@@ -1,3 +1,10 @@
+var without_basedir = url => {
+
+  if (url.substr(0, main.dirname.length)) {
+    return url.substr(main.dirname.length);
+  }
+
+};
 var require = (function() {
   // function join copied from creationix/path.js
   function join(/* path segments */) {
@@ -16,14 +23,24 @@ var require = (function() {
     return newParts.join("/") || (newParts.length ? "/" : ".");
   }
 
-  var modules = {};
+  // function helpers
+  var with_js = name => name + (name.substr(-3) == '.js' ? '' : '.js'),
+      without_js = name => name.substr(-3) == '.js'
+                         ? name.substr(0, name.length - 3)
+                         : name,
+      without_basedir = url => {
+        if (is_beggining_with(url, main.dirname)) {
+          return url.substr(main.dirname.length + 1);
+        }
+      },
+      is_beggining_with = (full, start) => (full.substr(0, start.length) == start);
 
   class Module {
-    constructor(url) {
+    constructor(filename) {
       this.loaded = false;
 
-      this.filename = url;
-      this.dirname = join(url, url.substr(-1) == '/' ? '.' : '..');
+      this.filename = filename;
+      this.dirname = join(filename, filename.substr(-1) == '/' ? '.' : '..');
 
       this.children = [];
       this.prepare = imports => null,
@@ -35,10 +52,10 @@ var require = (function() {
       module.parent = this;
     }
   }
-  modules.main = new Module(window.location.pathname);
 
-  // function to append .js to name  if necessary
-  var with_js = name => name + (name.substr(-3) == '.js' ? '' : '.js');
+  var main = new Module(window.location.pathname),
+    ajaxOpts = { cache: false, dataType: 'text' },
+    modules = {};
 
   // require function
   var fn = function(names) {
@@ -46,10 +63,10 @@ var require = (function() {
       return Promise.all(names.map(fn));
     } else if (typeof names === 'string') {
       var name = names;
-      var parent = this instanceof Module ? this : modules.main ;
+      var parent = this instanceof Module ? this : main ;
       var url = join(parent.dirname, with_js(name));
-      var module = modules[url];
-      var ajaxOpts = { cache: false, dataType: 'text' };
+      var identifier = without_js(without_basedir(url));
+      var module = modules[identifier];
 
       return new Promise(function(resolve, reject){
         if (module) {
@@ -59,7 +76,7 @@ var require = (function() {
             $(module).on('loaded', (e) => resolve(module.exports));
           }
         } else {
-          module = modules[url] = new Module(url);
+          module = modules[identifier] = new Module(url);
           parent.addChildren(module);
           // fetching module definition
           $.ajax(url, ajaxOpts).fail(reject)
@@ -84,7 +101,16 @@ var require = (function() {
       return new Promise((resolve, reject) => {resolve(names)});
     }
   }
-  fn.modules = modules;
+  fn.modules = () => modules;
+  fn.main = () => main;
+  fn.basedir = newBasedir => {
+    if (typeof newBasedir == 'string') {
+      main.dirname = join('/', newBasedir);
+      main.children = [];
+      modules = {};
+    };
+    return main.dirname;
+  };
   return fn;
 }({
 
